e45a3f2a51d0c2584f55dc0933e0554f1975e96c
VsevolodRun <vsievolod.florienskii.98@mail.ru>
2025-06-07 00:29:37 +0300

    Add files via upload
    Добавляю файлы выпускной квалификационной работы для проверки

---
+++ ML_FLASK_COMPOSITE/app.py
@@ -0,0 +1,26 @@
+import flask
+from flask import render_template
+import pickle
+import sklearn
+from sklearn.linear_model import LinearRegression
+
+app = flask.Flask(__name__, template_folder = 'templates')
+
+@app.route('/', methods = ['POST', 'GET'])
+
+@app.route('/index', methods = ['POST', 'GET'])
+def main():
+    if flask.request.method == 'GET':
+        return render_template('main.html')
+    
+    if flask.request.method == 'POST':
+        with open('lr_model.pkl', 'rb') as f:
+            loaded_model = pickle.load(f)
+
+        feat = float(flask.request.form['feature'])
+        y_pred = loaded_model.predict([[feat]])
+
+        return render_template('main.html', result = y_pred)
+    
+if __name__ == '__main__':
+    app.run(debug = True)
